5b1ff9a9954b68ffbf62212628caa7592242e2a5
vinicius.freitas00 <vini.freirefreitas00@gmail.com>
2021-12-08 16:55:46 -0300

    :sparkles: add summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import { GlobalStyle } from "./styles/globals";
+import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+
+import { GlobalStyle } from "./styles/globals";
 
 export function App() {
   return (
     <>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </>
   );
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import entradasImg from "../../assets/entradas.svg";
+import saidasImg from "../../assets/saidas.svg";
+import totalImg from "../../assets/total.svg";
+
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={entradasImg} alt="Entradas" />
+        </header>
+        <strong>R$1000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={saidasImg} alt="Saídas" />
+        </header>
+        <strong>- R$5000,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+        <strong>R$500,00</strong>
+      </div>
+    </Container>
+  );
+}
