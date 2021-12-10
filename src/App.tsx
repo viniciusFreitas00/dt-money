4afbf0500d41c0f81392cf0d653e6f1972d79cda
vinicius.freitas00 <vini.freirefreitas00@gmail.com>
2021-12-10 08:22:34 -0300

    :sparkles: creating transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/globals";
 
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+    console.log("dmsjhdjhsdjshu");
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
